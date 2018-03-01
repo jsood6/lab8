c9e3a042b88114d1533362dbaeeb455ffd04d81c
Jigyasaa Sood <jsood@ucsd.edu>
2018-03-01 13:06:46 -0800

    finished lab8

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,17 +10,54 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
+ 	initCamera();
  	initGestures();
  	initRSVPForm();
 }
 
+/*$(function(){
+    $("div.box").bind("taphold", tapholdHandler);
+    
+    function tapholdHandler(event){
+      //get id of the event source
+      
+      $(event.target).addClass("taphold");
+    }
+  });*/
+
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+
+	$(function(){
+		$(".judge-img").bind("taphold", tapholdHandler);
+		
+		function tapholdHandler(event){
+			//get id of the event source
+			var targetIDPrefix = event.target.id;
+			console.log("got prefix: "+ targetIDPrefix);
+			//show bio
+			$("#" + targetIDPrefix + "-bio").show();
+			//$(event.target).addClass("taphold");
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e){
+  	e.preventDefault();
+  	console.log("submitting form...");
+  	var rsvpEmail = $('#rsvpEmail').val();
+
+  	//send the POST request
+  	$.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  	console.log("at end of method1");
+  });
+
+  function postCallback(res){
+  	alert("RSVP form successfully submitted!");
+  	$('#rsvpEmail').val(''); //clear form
+  }
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,16 @@ var data = {
 /*
  * GET tasks page.
  */
+
 exports.adminView = function(req, res){
   res.render('rsvp', data);
-};
+}
+
+exports.addRSVP = function(req,res){
+	var rsvpEmail = req.body.rsvpEmail;
+
+	console.log(rsvpEmail);
+	data.rsvp.push(rsvpEmail);
+
+	res.send(rsvpEmail);
+}
